bbfff2a91e88a840c3a109e1f652082959a8164f
Sn0wF1re <mokayaclinton34@gmail.com>
2026-08-21 22:37:50 +0300

    fix: use auto-imported component names for navbar and footer in layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,14 +1,9 @@
-<script setup lang="ts">
-import AppNavbar from '~/components/global/AppNavbar.vue'
-import AppFooter from '~/components/global/AppFooter.vue'
-</script>
-
 <template>
   <div class="min-h-screen flex flex-col">
-    <AppNavbar />
+    <GlobalAppNavbar />
     <main class="flex-1 pt-20">
       <slot />
     </main>
-    <AppFooter />
+    <GlobalAppFooter />
   </div>
 </template>
